4a313fbc1a2474aaeb26893ce7dae9f7548a8954
arthurprs <arthurprs@gmail.com>
2015-10-19 12:00:54 -0200

    add cookie tests

--- src/cookie.rs
+++ src/cookie.rs
@@ -6,6 +6,7 @@ pub struct Cookie(u64);
 impl Cookie {
     #[inline(always)]
     pub fn new(token: Token, nonce: u64) -> Cookie {
+        // TODO: panic on overflow?
         Cookie((token.0 << 48) as u64 | (nonce & 0xFFFFFFFFFFFF))
     }
 
@@ -17,5 +18,27 @@ impl Cookie {
     #[inline(always)]
     pub fn nonce(self) -> u64 {
         self.0 & 0xFFFFFFFFFFFF
+    }
+}
+
+mod tests {
+    use super::Cookie;
+    use mio::Token;
+
+    #[test]
+    fn round_trip() {
+        let a = Cookie::new(Token(1), 1);
+        assert_eq!(a.token(), Token(1));
+        assert_eq!(a.nonce(), 1);
+        let a = Cookie::new(Token((1 << 16) - 1), (1 << 48) - 1);
+        assert_eq!(a.token(), Token((1 << 16) - 1));
+        assert_eq!(a.nonce(), (1 << 48) - 1);
+    }
+
+    #[test]
+    fn overflow() {
+        let a = Cookie::new(Token((1 << 16) + 1), (1 << 48) + 1);
+        assert_eq!(a.token(), Token(1));
+        assert_eq!(a.nonce(), 1);
     }
 }
