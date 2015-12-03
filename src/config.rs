d1a169eb97489e07970f20594c510a2fa4bb8822
arthurprs <arthurprs@gmail.com>
2015-12-03 18:15:04 -0200

    improved code for reading configuration

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,9 @@
 use std::fs::{self, File};
 use std::path::PathBuf;
+use std::io;
 use std::io::prelude::*;
 use toml::Parser as TomlParser;
+use utils::*;
 
 pub const TMP_QUEUE_CHECKPOINT_FILE: &'static str = "queue.checkpoint.tmp";
 pub const QUEUE_CHECKPOINT_FILE: &'static str = "queue.checkpoint";
@@ -15,10 +17,8 @@ pub struct ServerConfig {
     pub data_directory: PathBuf,
     pub bind_address: String,
     pub max_connections: usize,
-    pub segment_size: u64,
     pub maintenance_interval: u64,
-    pub message_timeout: u32,
-    pub retention_period: u32,
+    default_queue_config: QueueConfig,
 }
 
 #[derive(Debug)]
@@ -28,6 +28,9 @@ pub struct QueueConfig {
     pub segment_size: u64,
     pub message_timeout: u32,
     pub retention_period: u32,
+    pub hard_retention_period: u32,
+    pub retention_size: u64,
+    pub hard_retention_size: u64,
 }
 
 
@@ -65,39 +68,69 @@ pub fn parse_size(size_text: &str) -> Result<u64, ()> {
     number.checked_mul(scale).ok_or(())
 }
 
+macro_rules! read_config {
+    ($config: expr, $name: expr, $function: ident, $ty: expr) => {
+        $config.get($name).expect(concat!("Config ", $name, " not found"))
+            .$function().expect(concat!("Config ", $name, " expected to be of ", $ty, " type"))
+    };
+    ($config: expr, $name: expr => str) => {
+        read_config!($config, $name, as_str, "string")
+    };
+    ($config: expr, $name: expr => int) => {
+        read_config!($config, $name, as_integer, "integer")
+    };
+    ($config: expr, $name: expr => size) => {
+        parse_size(read_config!($config, $name, as_str, "size")).
+            expect(concat!("Config ", $name, " can't be parsed as size"))
+    };
+    ($config: expr, $name: expr => duration) => {
+        parse_duration(read_config!($config, $name, as_str, "duration")).
+            expect(concat!("Config ", $name, " can't be parsed as duration"))
+    }
+}
+
 impl ServerConfig {
     pub fn read() -> ServerConfig {
         debug!("reading config");
 
         let config = {
             let mut s = String::new();
-            File::open("floki.toml").unwrap().read_to_string(&mut s).unwrap();
-            TomlParser::new(&s).parse().unwrap()
+            File::open("floki.toml").expect("Error opening config file").
+                read_to_string(&mut s).expect("Error reading config file");
+            TomlParser::new(&s).parse().expect("Error parsing config file")
         };
         info!("done reading config: {:?}", config);
 
-        let bind_address = config.get("bind_address").unwrap().as_str().unwrap();
-        let data_directory = config.get("data_directory").unwrap().as_str().unwrap();
-        let max_connections = config.get("max_connections").unwrap().as_integer().unwrap();
-        let segment_size = parse_size(
-            config.get("segment_size").unwrap().as_str().unwrap()).unwrap();
-        let maintenance_interval = parse_duration(
-            config.get("maintenance_interval").unwrap().as_str().unwrap()).unwrap();
-        let message_timeout = parse_duration(
-            config.get("message_timeout").unwrap().as_str().unwrap()).unwrap();
-        let retention_period = parse_duration(
-            config.get("retention_period").unwrap().as_str().unwrap()).unwrap();
+        let bind_address = read_config!(config, "bind_address" => str);
+        let data_directory = read_config!(config, "data_directory" => str);
+        let max_connections = read_config!(config, "data_directory" => int);
+        let segment_size = read_config!(config, "segment_size" => size);
+        let maintenance_interval = read_config!(config, "maintenance_interval" => duration);
+        let message_timeout = read_config!(config, "message_timeout" => duration);
+        let retention_period = read_config!(config, "retention_period" => duration);
+        let hard_retention_period = read_config!(config, "hard_retention_period" => duration);
+        let retention_size = read_config!(config, "retention_size" => size);
+        let hard_retention_size = read_config!(config, "hard_retention_size" => size);
 
-        assert!(segment_size <= 1 << 31, "segment_size must be <= 2GB");
+        assert!(segment_size >= 16 * 1024 * 1024 && segment_size <= 1 << 31,
+            "segment_size must be between 16MB and 2GB");
+        create_dir_if_not_exist(data_directory).expect("Data directory not acessible");
 
         ServerConfig {
             data_directory: data_directory.into(),
             bind_address: bind_address.into(),
             max_connections: max_connections as usize,
-            segment_size: segment_size,
             maintenance_interval: maintenance_interval,
-            message_timeout: (message_timeout / 1000) as u32,
-            retention_period: (retention_period / 1000) as u32,
+            default_queue_config: QueueConfig {
+                name: "".into(),
+                data_directory: "".into(),
+                segment_size: segment_size,
+                message_timeout: (message_timeout / 1000) as u32,
+                retention_period: (retention_period / 1000) as u32,
+                hard_retention_period : (hard_retention_period / 1000) as u32,
+                retention_size : retention_size,
+                hard_retention_size: hard_retention_size,
+            }
         }
     }
 
@@ -130,9 +163,7 @@ impl QueueConfig {
         QueueConfig {
             name: name,
             data_directory: data_directory,
-            segment_size: server_config.segment_size,
-            message_timeout: server_config.message_timeout,
-            retention_period: server_config.retention_period,
+            .. server_config.default_queue_config
         }
     }
 
@@ -141,5 +172,8 @@ impl QueueConfig {
         Self::new(server_config, name)
         // TODO: load from a file instead
     }
-}
 
+    fn write(&self) -> Result<(), io::Error> {
+        Ok(())
+    }
+}
